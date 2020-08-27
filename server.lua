8045a8e2bf766d9a12f0cf66679b5687ea192b88
Ediz <56892048+ediz9@users.noreply.github.com>
2020-08-27 17:23:19 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,7 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
 ESX.RegisterUsableItem('wallet', function(source)
     local _source = source
     TriggerClientEvent('edz:wallet:client', _source)
